e733433809b34d65c31d0d8012ccf3197566c162
awong50 <aidanwonglam@gmail.com>
2026-08-02 19:12:47 -0400

    Added controlled sampling tests

--- adc_tests/sampling/findings.md
+++ adc_tests/sampling/findings.md
@@ -3,7 +3,7 @@
 ## Maximum ADC Acquisition Rate
 
 ### Test Setup
-- Sample count: 100,000 samples
+- Sample count: 100,000 
 - Sampling method: Software polling
 
 ### Results
@@ -13,3 +13,22 @@
 ### Notes
 - Measured acqusition rate is likely lower due to acquisiton method: software overhead from ADC polling adds time
 - Tested higher and lower sample counts - Results are the same
+
+## Controlled Sampling 
+
+### Test Setup
+- Sampling method: Software-controlled sampling using `sleep_us()`
+- Sample count: 10,000 
+
+### Results
+
+| Requested Sampling Rate | Measured Sampling Rate | Accuracy |
+|-------------------------|------------------------|----------|
+| 1 kHz                   |        998.002         |  99.8%   |
+| 10 kHz                  |        9803.82         |   98%    |
+| 50 kHz                  |        45454.1         |   91%    |
+| 100 kHz                 |        83331.9         |   83.3%  |
+
+### Notes
+- The measured sampling rate is lower than the requested rate due to software overhead (instead of just the period of the sleep and ADC, there is also the code time)
+- As the requested sample rate increases, the accuracy decreases because ADC conversion time and software execution overhead becomes a larger portion of the total sampling period
--- adc_tests/sampling/main.cpp
+++ adc_tests/sampling/main.cpp
@@ -28,6 +28,24 @@ double max_acquisition_rate(uint64_t samples) {
     return samples / (absolute_time_diff_us(start_time, end_time) / 1000000.0);
 }
 
+// Accepts the amount of ADC samples to collect and the requested sample rate in Hz; Outputs the actual rate
+double controlled_sampling_rate(uint64_t samples, double requested_rate) {
+
+    // f = 1/T -> T = 1/f -> T = 1000000 / f (for microseconds)
+    uint64_t sample_period_us = 1000000.0 / requested_rate;
+
+    absolute_time_t start_time = get_absolute_time();
+
+    for (uint64_t i = 0; i < samples; i++) {
+        get_voltage();
+        sleep_us(sample_period_us);
+    }
+
+    absolute_time_t end_time = get_absolute_time();
+
+    return samples / (absolute_time_diff_us(start_time, end_time) / 1000000.0);
+}
+
 int main() {
     stdio_init_all();
 
@@ -39,11 +57,32 @@ int main() {
     adc_select_input(0);
 
     while (1) {
-        uint64_t samples = 10000000;
+
+        /*
+        // Max Acquisition Rate Test
+
+        uint64_t samples = 100000;
         float max_rate = max_acquisition_rate(samples);
         cout << "Maximum Acquisition Rate: " << max_rate << " kS/s" << endl;
 
         sleep_ms(1000);
+        */
+    
+        // Controlled Sampling Test
+
+        while (1) {
+            uint64_t samples = 10000;
+            double requested_rate = 100000; // 50 kHz
+
+            double actual_rate = controlled_sampling_rate(samples, requested_rate);
+
+            cout << "Requested: " << requested_rate << endl;    
+            cout << "Actual: " << actual_rate << endl;
+            cout << "Percentage of Requested: " << actual_rate / requested_rate * 100 << "%" << endl;
+
+            sleep_ms(1000);
+        }
+
     }    
     
 }
